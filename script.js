0562c6b388cc7c6b85ddb54e929d9b8a555cad20
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-06-02 08:50:21 -0300

    feat: :egg: display none em span

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ document.addEventListener("keydown", function (event) {
         konamiCodePosition++;
         if (konamiCodePosition === konamiCode.length) {
             var dataSpan = document.getElementById("hm")
-            dataSpan.textContent = atob("d2Vic2l0ZXMgbGlrZSB0aGlzIGFyZSBraW5kYSBsaWtlIGZ1dHVyZSB0b21ic3RvbmVzLgpiZSBuZWNlc3NhcnkuIGRvbid0IGJlIGEgcGxhY2Vob2xkZXIuIHJlZnVzZSB0byBiZSB3b3JzZS4gb3Bwb3NlIG9wcmVzc2lvbi4Kc2hhcmUgc29uZ3MuIHRhbGsgYWJvdXQgaXQuIHRoaW5rIGFib3V0IGl0Lg==")
+            dataSpan.textContent = atob("d2Vic2l0ZXMgbGlrZSB0aGlzIGFyZSBraW5kYSBsaWtlIGZ1dHVyZSB0b21ic3RvbmVzLiBiZSBuZWNlc3NhcnkuIGRvbid0IGJlIGEgcGxhY2Vob2xkZXIuIHJlZnVzZSB0byBiZSB3b3JzZS4gb3Bwb3NlIG9wcHJlc3Npb24uIHNoYXJlIHNvbmdzLiB0YWxrIGFib3V0IGl0LiB0aGluayBhYm91dCBpdC4=")
+            dataSpan.style.display = "";
             konamiCodePosition = 0;
         }
     } else {
