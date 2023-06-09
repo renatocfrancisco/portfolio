f2d528c4fa6c4fc5a7acdb617d0ac47fba2cd3ff
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-06-09 15:35:08 -0300

    refactor: :recycle: renomear variavel em script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const konamiCode = [
+const code = [
     "ArrowUp",
     "ArrowUp",
     "ArrowDown",
@@ -10,18 +10,18 @@ const konamiCode = [
     "b",
     "a",
 ];
-let konamiCodePosition = 0;
+let codePosition = 0;
 document.addEventListener("keydown", function (event) {
     const key = event.key;
-    if (key === konamiCode[konamiCodePosition]) {
-        konamiCodePosition++;
-        if (konamiCodePosition === konamiCode.length) {
+    if (key === code[codePosition]) {
+        codePosition++;
+        if (codePosition === code.length) {
             var dataSpan = document.getElementById("hm")
             dataSpan.textContent = atob("d2Vic2l0ZXMgbGlrZSB0aGlzIGFyZSBraW5kYSBsaWtlIGZ1dHVyZSB0b21ic3RvbmVzLiBiZSBuZWNlc3NhcnkuIGRvbid0IGJlIGEgcGxhY2Vob2xkZXIuIHJlZnVzZSB0byBiZSB3b3JzZS4gb3Bwb3NlIG9wcHJlc3Npb24uIHNoYXJlIHNvbmdzLiB0YWxrIGFib3V0IGl0LiB0aGluayBhYm91dCBpdC4=")
             dataSpan.style.display = "";
-            konamiCodePosition = 0;
+            codePosition = 0;
         }
     } else {
-        konamiCodePosition = 0;
+        codePosition = 0;
     }
 });
