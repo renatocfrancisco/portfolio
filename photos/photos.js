1f829ec969f8342c42e468d30ebf0b89726ad9fa
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-06-09 15:36:05 -0300

    feat: :egg: span #hm

---
+++ photos/photos.js
@@ -0,0 +1,41 @@
+function playAudio() {
+    var audio = new Audio('assets/morse.wav');
+    audio.volume = 0.00119;
+    setTimeout(function () {
+        try {
+            audio.play();
+        } catch (error) {
+            console.error(error);
+        }
+    }, 1000);
+}
+
+playAudio();
+
+const code1 = [
+    "9",
+    "1",
+    "1"
+];
+
+const code2 = [
+    "1",
+    "9",
+    "0"
+];
+
+let codePosition = 0;
+document.addEventListener("keydown", function (event) {
+    const key = event.key;
+    if (key === code1[codePosition] || key === code2[codePosition]) {
+        codePosition++;
+        if (codePosition === code1.length || codePosition === code2.length) {
+            var dataSpan = document.getElementById("hm")
+            dataSpan.textContent = 'XD';
+            dataSpan.style.display = "";
+            codePosition = 0;
+        }
+    } else {
+        codePosition = 0;
+    }
+});
